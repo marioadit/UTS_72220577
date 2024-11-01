e0e2ceafe8dfcb7fe1b657c75d0d99f42a8c7f1e
marioadit <marioadityakurnianto@gmail.com>
2024-11-01 23:13:07 +0700

    courses nav

--- UTS_72220577/Pages/EditCourses.xaml.cs
+++ UTS_72220577/Pages/EditCourses.xaml.cs
@@ -63,7 +63,7 @@ namespace UTS_72220577.Pages
 
                 await _ccService.UpdateCourseAsync(updatedCourse);
                 await DisplayAlert("Success", "Course updated successfully.", "OK");
-                await Navigation.PopAsync();
+                await Navigation.PushAsync(new Courses());
             }
             else
             {
